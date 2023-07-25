f8ddcc293de3d0d95a521fe4187533981e1b9b87
TPODAvia <thepowerofdarknes2000@gmail.com>
2023-07-25 18:06:34 +0300

    Imu add

--- my_cpp_py_pkg/external_scripts/i2c.py
+++ my_cpp_py_pkg/external_scripts/i2c.py
@@ -6,7 +6,7 @@ address = 0x5c  # I2C address of the device
 
 while True:
     data = bus.read_byte(address)  # Read a single byte from the device
-    gyro_data = bus.read_i2c_block_data(0x68, 0, 7)
-    print(gyro_data)
-    # print(data)
-    time.sleep(1)
+    # gyro_data = bus.read_i2c_block_data(0x5c, 0, 7)
+    # print(gyro_data)
+    print(data)
+    time.sleep(0.1)
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 ORB SLAM 3: https://github.com/zang09/ORB_SLAM3_ROS2
 
+https://github.com/jefmenegazzo/mpu-i2c-drivers-python
 
 sudo apt-get install -y i2c-tools python3-smbus
 sudo adduser ubuntu i2c

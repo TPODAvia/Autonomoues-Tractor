912147bb44164e839723abe9a8efcaf843a8a951
TPODAvia <thepowerofdarknes2000@gmail.com>
2023-07-25 16:20:12 +0300

    Folder update

---
+++ my_cpp_py_pkg/launch/web_control.launch.py
@@ -0,0 +1,33 @@
+# ros2 run teleop_twist_keyboard teleop_twist_keyboard
+# http://10.100.190.77:8080/
+import os
+ 
+from launch import LaunchDescription
+from launch_ros.actions import Node
+ 
+def generate_launch_description():
+ 
+ 
+ 
+    return LaunchDescription([
+        Node(
+            package='web_video_server',
+            executable='web_video_server',
+            name='web_video_server',
+            output='screen',
+        ),
+        Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+            output='screen',
+            parameters=[{
+                'image_size': [640,480],
+                'camera_frame_id': 'camera_link_optical'
+                }]
+        ),
+        Node(
+            package='my_cpp_py_pkg',
+            executable='cpp_executable',
+            name='my_node'
+        )
+    ])

e4c382b6b5629489062009d86b181c48587faf00
TPODAvia <thepowerofdarknes2000@gmail.com>
2023-08-29 15:22:30 +0000

    Web threading

--- main_pkg/scripts/web.py
+++ main_pkg/scripts/web.py
@@ -3,9 +3,11 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import _thread as thread
+import threading
+import time
 
 global_data = "Nondefverfve"
+shutdown = False
 
 class WebPublisherNode(Node):
     def __init__(self):
@@ -26,6 +28,7 @@ class WebPublisherNode(Node):
 
 class WebRequestHandler(BaseHTTPRequestHandler):
     latest_data = None
+
     def do_GET(self):
         if self.path == '/':
             global global_data
@@ -35,7 +38,7 @@ class WebRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(b'<html><body>')
             self.wfile.write(b'<h1>ROS2 Web Publisher</h1>')
             self.wfile.write(b'<p>Received data:</p>')
-            self.wfile.write(bytes(f'<p>{global_data}</p>', 'utf-8'))
+            self.wfile.write(bytes(f'<p>{WebRequestHandler.latest_data}</p>', 'utf-8'))
             self.wfile.write(b'</body></html>')
         else:
             self.send_response(404)
@@ -49,15 +52,28 @@ def start_server():
     server_address = ('', 8000)
     httpd = HTTPServer(server_address, WebRequestHandler)
     httpd.RequestHandlerClass = WebRequestHandler  # Set the request handler class explicitly
-    httpd.serve_forever()
+    # while not shutdown:
+    #     httpd.handle_request()
+    # httpd.serve_forever()
+    while not shutdown:
+        httpd.handle_request()
+        time.sleep(1)  # Adjust the sleep interval as needed
+    httpd.server_close()
 
 def main(args=None):
+    global shutdown
     rclpy.init(args=args)
     node = WebPublisherNode()
-    thread.start_new_thread(start_server, ())
-
-    node.destroy_node()
-    rclpy.shutdown()
+    server_thread = threading.Thread(target=start_server)
+    server_thread.start()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        shutdown = True
+        server_thread.join()
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()

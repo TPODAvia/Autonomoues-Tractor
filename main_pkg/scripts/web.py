84fc46dbeb1066e73a5218b7bbb0cb6880d6dec2
Minh Duke <64729573+TPODAvia@users.noreply.github.com>
2023-09-07 19:35:06 +0300

    Update web.py

--- main_pkg/scripts/web.py
+++ main_pkg/scripts/web.py
@@ -1,85 +1,103 @@
-#!/usr/bin/env python3
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import String
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import threading
-import time
+#!/usr/bin/env python3 
+import rclpy  
+from rclpy.node import Node  
+from std_msgs.msg import String  
+from sensor_msgs.msg import NavSatFix, Imu  
+from flask import Flask, jsonify  
+import threading  
+import time  
+from flask_cors import CORS, cross_origin
 
-from sensor_msgs.msg import NavSatFix, Imu
-
-shutdown = False
-
-class WebPublisherNode(Node):
-    def __init__(self):
-        super().__init__('web_publisher')
-        self.subscription_gps = self.create_subscription(NavSatFix, 'gps/filtered', self.gps_callback, 10)
-        self.subscription_imu = self.create_subscription(Imu, 'imu', self.imu_callback, 10)
-
-    def gps_callback(self, msg):
-        # pos_cov = [str(i) for i in msg.position_covariance]
-        # self.get_logger().info('Received: "%s"' % msg.position_covariance)
-        global_data = f"GPS: [Lat, Long, Alt]=[{msg.latitude},{msg.longitude},{msg.altitude}]"
-        WebRequestHandler.update_latest_data(global_data)
-
-    def imu_callback(self, msg):
-        global_data = f"IMU: Linear Acceleration [XYZ]=[{msg.linear_acceleration.x},{msg.linear_acceleration.y},{msg.linear_acceleration.z}] \
-        Quaternion [x, y, z, w]=[{msg.orientation.x},{msg.orientation.y},{msg.orientation.z},{msg.orientation.w}]"
-        WebRequestHandler.update_latest_data(global_data)
-
-class WebRequestHandler(BaseHTTPRequestHandler):
-    latest_gps_data = None
-    latest_imu_data = None
-
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(b'<html><body>')
-            self.wfile.write(b'<h1>ROS2 Web Publisher</h1>')
-            self.wfile.write(b'<p>Received data:</p>')
-            self.wfile.write(bytes(f'<p>{WebRequestHandler.latest_gps_data}</p>', 'utf-8'))
-            self.wfile.write(bytes(f'<p>{WebRequestHandler.latest_imu_data}</p>', 'utf-8'))
-            self.wfile.write(b'</body></html>')
-        else:
-            self.send_response(404)
-            self.end_headers()
-
-    @classmethod
-    def update_latest_data(cls, data):
-        # print(f"Received data update: {data}")  # Debug log message
-        if data.startswith("GPS"):
-            cls.latest_gps_data = data
-        if data.startswith("IMU"):
-            cls.latest_imu_data = data
-
-def start_server():
-    server_address = ('', 8000)
-    httpd = HTTPServer(server_address, WebRequestHandler)
-    httpd.RequestHandlerClass = WebRequestHandler  # Set the request handler class explicitly
-    # while not shutdown:
-    #     httpd.handle_request()
-    # httpd.serve_forever()
-    while not shutdown:
-        httpd.handle_request()
-        time.sleep(1)  # Adjust the sleep interval as needed
-    httpd.server_close()
-
-def main(args=None):
-    global shutdown
-    rclpy.init(args=args)
-    node = WebPublisherNode()
-    server_thread = threading.Thread(target=start_server)
-    server_thread.start()
+shutdown = False  
+  
+app = Flask(__name__)
+cors = CORS(app)
+latest_gps_data = None 
+latest_imu_data = None 
+  
+class WebPublisherNode(Node):  
+ 
+    def __init__(self):  
+        super().__init__('web_publisher')  
+        self.subscription_gps = self.create_subscription(NavSatFix, 'gps/filtered', self.gps_callback, 10)  
+        self.subscription_imu = self.create_subscription(Imu, 'imu', self.imu_callback, 10)  
+  
+    def gps_callback(self, msg):  
+        pos_cov = [str(i) for i in msg.position_covariance]  
+        global_data = {  
+            'data_GPS': {  
+                'latitude': msg.latitude,  
+                'longitude': msg.longitude,  
+                'altitude': msg.altitude,  
+                'position_covariance': pos_cov  
+            }  
+        }  
+        global latest_gps_data 
+        latest_gps_data = global_data 
+  
+    def imu_callback(self, msg):  
+        global_data = {  
+            'data_IMU': {  
+                    'x': msg.linear_acceleration.x,  
+                    'y': msg.linear_acceleration.y,  
+                    'z': msg.linear_acceleration.z, 
+                    'qx':msg.orientation.x, 
+                    'qy':msg.orientation.y, 
+                    'qz':msg.orientation.z, 
+                    'qw':msg.orientation.w 
+            }  
+        } 
+        global latest_imu_data 
+        latest_imu_data = global_data  
+         
+  
+ 
+@app.route('/')  
+def get_latest_data():  
+    global latest_gps_data  
+    global latest_imu_data
     try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        shutdown = True
-        server_thread.join()
-    finally:
-        node.destroy_node()
-        rclpy.shutdown()
+        if {**latest_gps_data, **latest_imu_data} is not None:  
+            return jsonify({**latest_gps_data, **latest_imu_data})  
+        else:  
+            return jsonify({'message': 'No data available'})
+    except:
+        return jsonify({'data_GPS': {
+                'latitude': -1,
+                'longitude': -1,
+                'altitude': -1,
+                'position_covariance': [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+            },
+            'data_IMU': {
+                    'x': 0,
+                    'y': 0,
+                    'z': 0,
+                    'qx':0,
+                    'qy':0,
+                    'qz':0,
+                    'qw':0
+            }
+	})
 
-if __name__ == '__main__':
+
+def start_flask_server():  
+    app.run(host='0.0.0.0', port=8000, threaded=True)  
+
+def main(args=None):  
+    global shutdown  
+    rclpy.init(args=args)  
+    node = WebPublisherNode()  
+    server_thread = threading.Thread(target=start_flask_server)
+    server_thread.setDaemon(True)  
+    server_thread.start()  
+    try:  
+        rclpy.spin(node)  
+    except KeyboardInterrupt:  
+        shutdown = True  
+        exit(0)  
+    finally:  
+        node.destroy_node()  
+        rclpy.shutdown()  
+  
+if __name__ == '__main__':  
     main()
